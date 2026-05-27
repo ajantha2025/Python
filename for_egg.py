00dfd4a021a756e5855fe6054a0f471f79d12e88
ajantha2025 <majantha13@gmail.com>
2026-05-27 15:13:31 -0500

    for loop

---
+++ for_egg.py
@@ -0,0 +1,5 @@
+eggs = int(input("enter number of eggs:"))
+flour = 0
+for egg in range(eggs):
+    flour +=2
+    print("toal flour needed",flour,"cups")
---
+++ list.py
@@ -0,0 +1,8 @@
+colours = ["pink","blue","yellow"]
+print(colours[2])
+colours.append("brown")
+print(colours)
+colours[1] = "orange" 
+print(colours)
+colours.remove("brown")
+print(colours)
